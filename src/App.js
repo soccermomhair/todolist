ffd18b17194ad932ce1f10e8c5e4f7ac516a5bf9
lxschutz <lxschutz@gmail.com>
2023-06-25 17:44:05 -0500

    all in one file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tlist, setTlist] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (task === "") {
+      return;
+    }
+    const newTask = {
+      assignment: task,
+      completed: false
+    }
+    setTlist([...tlist, newTask]);
+
+    setTask("");
+  }
+
+  const deleteItem = (delIdx) => {
+    setTlist(tlist.filter((task, index) =>
+      index !== delIdx));
+  }
+
+  const handleCheckbox = (index) => {
+    const updatedlist = tlist.map((task, idx) => {
+      if (idx === index) {
+        task.completed = !task.completed;
+        // const updatedlist = { ...task, completed: !task.completed };
+      }
+
+      return task;
+    });
+    setTlist(updatedlist);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={(event) => { handleSubmit(event) }}>
+        <div>
+          <label htmlFor="assignment">Enter your task:</label>
+          <input type="text" id="assignment" name="assignment" value={task} onChange={(event) => { setTask(event.target.value) }} />
+        </div>
+
+        <button>Submit</button>
+      </form>
+
+
+      {
+        tlist.map((task, index) => (
+
+          <div key={index}>
+            <span>{task.assignment}</span>
+            <input type="checkbox" checked={task.completed} onChange={(event) =>
+              handleCheckbox(index)} />
+            <button onClick={(event) => { deleteItem(index) }}>Delete</button>
+          </div>
+        ))}
     </div>
-  );
+
+  )
+
 }
+
 
 export default App;
